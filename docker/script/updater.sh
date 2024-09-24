b0ef8f458863d05151e1ba13750e48f65f19eb34
Babouche <clnabergel@hotmail.fr>
2024-09-24 17:59:46 -0400

    Create updater.sh

---
+++ docker/script/updater.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+COMPOSE_FILE_PATH="./docker-compose.yml"
+
+if [ ! -f "$COMPOSE_FILE_PATH" ]; then
+        echo "docker-compose.yml not found at $COMPOSE_FILE_PATH"
+        exit 1
+fi
+
+echo "Now updating container."
+
+docker-compose down
+docker-compose up --force-recreate -d
+
+echo "Erasing oldest image."
+docker image prune -f
+
+echo "Buggybot-Dashboard has been successfully updated."
